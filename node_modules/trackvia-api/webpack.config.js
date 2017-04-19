d5ae6833081612d5791862908bba5fec38f4e236
John Etherton <john.etherton@trackvia.com>
2017-04-19 10:43:38 -0600

    initial commit

---
+++ node_modules/trackvia-api/webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    target: 'node',
+    context: __dirname,
+    entry: {
+        'trackvia-api': './src/trackvia-api'
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: '[name].js',
+        library: 'trackvia-api',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json']
+    },
+    node: {
+        net: 'empty',
+        fs: 'empty',
+        tls: 'empty'
+    }
+};
